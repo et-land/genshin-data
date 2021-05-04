d7b8208a2e79b605f7f11092f351af1d41d4d04e
Alex Fang <deeno@qq.com>
2021-05-05 06:44:10 +0800

    chore(eslint): add eslint

--- scripts/cli.js
+++ scripts/cli.js
@@ -1,9 +1,9 @@
-const Yargs = require('yargs');
-const Path = require('path');
+const Yargs = require('yargs')
+const Path = require('path')
 
-global.base = Path.resolve(__dirname, '../');
+global.base = Path.resolve(__dirname, '../')
 
-Yargs(process.argv)
-    .demandCommand()
-    .help()
-    .argv;
+Yargs(process.argv) /* eslint-disable-line */
+  .demandCommand()
+  .help()
+  .argv
